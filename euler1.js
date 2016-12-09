0c558a5307eaa680ff2ab3e36c02cf67f30000c0
kboygit <heybisayangdako@gmail.com>
2016-12-09 09:15:12 -0800

    Made a function clickme , modified js nad html

--- euler1.js
+++ euler1.js
@@ -2,12 +2,16 @@
 
 Find the sum of all the multiples of 3 or 5 below 1000. */
 
+function clickme(){
 var multiplesOfX = 0 ;
+var x = document.getElementById("userInput").value;
 
 for (x = 0 ; x < 1000 ; x++) {
   if (x % 3 === 0 || x % 5 ===0){
     multiplesOfX+= x;
   }
-};
+}
 
-console.log(multiplesOfX);
+document.getElementById("outout").innerHTML = multiplesOfX;
+
+}
